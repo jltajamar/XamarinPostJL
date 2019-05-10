69c62ed3c3f002b5c77303277f27834abc9adcb9
Juan Luis Sánchez Ángeles <JuanLuis.Sanchez@tajamar365.com>
2019-05-10 11:48:05 +0200

    Agregar archivos de proyecto.

---
+++ XamarinPostJL/XamarinPostJL/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Plugin.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinPostJL
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            this.BtnCall.Clicked += BtnCall_Clicked;
+        }
+
+        private void BtnCall_Clicked(object sender, EventArgs e)
+        {
+            var call = CrossMessaging.Current.PhoneDialer;
+
+            if (call.CanMakePhoneCall)
+                call.MakePhoneCall(PhoneNumber.Text);
+
+            //throw new NotImplementedException();
+        }
+
+       
+    }
+}
