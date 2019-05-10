a86cb50a7162bfec17af9d1c84023c817f5c1c3c
Juan Luis Sánchez Ángeles <JuanLuis.Sanchez@tajamar365.com>
2019-05-10 13:56:04 +0200

    mg

---
+++ XamarinPostJL/XamarinPostJL/SmsPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Plugin.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinPostJL
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SmsPage : ContentView
+	{
+		public SmsPage ()
+		{
+			InitializeComponent ();
+            this.BtnSendSMS.Clicked += SendSMS;
+        }
+
+        private void SendSMS(object sender, EventArgs e)
+        {
+            var call = CrossMessaging.Current.PhoneDialer;
+
+            if (call.CanSendSms)
+                call.SendSms(SMSPhoneNumber.Text, SMSBody.Text);
+
+            //throw new NotImplementedException();
+        }
+
+    }
+}
